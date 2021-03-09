e4eb9a61fb3d884067cde09a3a05aa162e30e871
Joe <joedinnsurrey@gmail.com>
2021-03-09 18:17:29 +0000

    exp

--- exp.py
+++ exp.py
@@ -91,7 +91,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--base_cfg",
-        default="configs/sign_recognition_chalearn_openpose.yaml",
+        default="openpose",
         type=str,
         help="Base config file (yaml).",
     )
